26d9b25a686a99ecd58967482411a07374346e86
sabasaikyouno <0852kuro@gmail.com>
2022-12-28 15:37:53 +0900

    t r

--- test/controllers/JsonControllerSpec.scala
+++ test/controllers/JsonControllerSpec.scala
@@ -4,14 +4,16 @@ import models.posted.PostedData
 import models.room.RoomData
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
-import play.api.libs.json.{JsPath, Json}
+import play.api.libs.json.Json
 import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
 
 
 class JsonControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting {
+
+  private val login = route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona"))).get
+  private val loginJson = contentAsJson(login).as[Map[String, String]]
 
   "JsonController Test" should {
 
@@ -22,15 +24,11 @@ class JsonControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injectin
     }
 
     "login" in {
-      val login = route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona"))).get
-
       status(login) mustBe OK
       contentAsJson(login).as[Map[String, String]].isDefinedAt("id") mustBe true
     }
 
     "createRoom" in {
-      val login = route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona"))).get
-      val loginJson = contentAsJson(login).as[Map[String, String]]
       val createRoom = route(app, FakeRequest(POST, "/json/room").withJsonBody(Json.obj("title" -> "testTitle")).withHeaders("id" -> loginJson("id"))).get
 
       status(createRoom) mustBe OK
@@ -39,8 +37,6 @@ class JsonControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injectin
 
     "room" in {
       for {
-        login <- route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona")))
-        loginJson = contentAsJson(login).as[Map[String, String]]
         createRoom <- route(app, FakeRequest(POST, "/json/room").withJsonBody(Json.obj("title" -> "testTitle")).withHeaders("id" -> loginJson("id")))
         createRoomJson = contentAsJson(createRoom).as[Map[String, String]]
         room <- route(app, FakeRequest(GET, s"/json/room/${createRoomJson("roomId")}").withHeaders("id" -> loginJson("id")))
