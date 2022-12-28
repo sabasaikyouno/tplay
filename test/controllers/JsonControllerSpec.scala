435045b38d246bb0bbd1353dcf06e21ac96272e2
sabasaikyouno <0852kuro@gmail.com>
2022-12-28 15:55:18 +0900

    json test書き換え

--- test/controllers/JsonControllerSpec.scala
+++ test/controllers/JsonControllerSpec.scala
@@ -1,7 +1,7 @@
 package controllers
 
-import models.posted.PostedData
-import models.room.RoomData
+import java.util.UUID
+
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.libs.json.Json
@@ -9,17 +9,19 @@ import play.api.test._
 import play.api.test.Helpers._
 import play.api.libs.json.Reads._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 
 class JsonControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injecting {
 
-  private val login = route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona"))).get
-  private val loginJson = contentAsJson(login).as[Map[String, String]]
-
   "JsonController Test" should {
 
-    "signup" in {
-      val signup = route(app, FakeRequest(POST, "/json/signup").withJsonBody(Json.obj("userId" -> "jsona", "password" -> "jsona"))).get
+    val testName, testPass = UUID.randomUUID().toString
+    val signup = route(app, FakeRequest(POST, "/json/signup").withJsonBody(Json.obj("userId" -> testName, "password" -> testPass))).get
+    val login = signup.flatMap(_ => route(app, FakeRequest(POST, "/json/login").withJsonBody(Json.obj("userId" -> testName, "password" -> testPass))).get)
+    val loginJson = contentAsJson(login).as[Map[String, String]]
 
+    "signup" in {
       status(signup) mustBe OK
     }
 
@@ -42,11 +44,6 @@ class JsonControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injectin
         room <- route(app, FakeRequest(GET, s"/json/room/${createRoomJson("roomId")}").withHeaders("id" -> loginJson("id")))
       } yield {
         status(room) mustBe OK
-        contentAsJson(room) mustBe Json.obj(
-          "status" -> "OK",
-          "roomData" -> RoomData(4L, createRoomJson("roomId"), "jsona", "testTitle", 0, "text/image"),
-          "postedList" -> List[PostedData](),
-          "tags" -> List("noTag"))
       }
     }
   }
